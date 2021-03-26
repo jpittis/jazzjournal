a53db9337584a0781b10daa692528489dab7c785
Jake Pittis <jakepittis@gmail.com>
2021-03-25 22:23:25 -0400

    Init

--- build.rb
+++ build.rb
@@ -15,6 +15,13 @@ timestamps = YAML.load_file("timestamps.yaml")
 renderer   = ERB.new(File.read("index.html.erb"))
 params     = Params.new(timestamps)
 
+timestamps.each do |ts|
+  if time = ts["ts"]
+    parts = time.split(':')
+    ts["ms"] = parts[0].to_i * 60_000 + parts[1].to_i * 1_000
+  end
+end
+
 File.open("index.html", "w") do |f|
   f.write(renderer.result(params.get_binding))
 end
